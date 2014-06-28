71ab5e9aee601d285e97c3df85b88f60e484285e
Joseph Kania <jkania@andrew.cmu.edu>
2014-06-28 09:35:23 -0400

    Further improvements to missing file handling

--- run_calgary.sh
+++ run_calgary.sh
@@ -3,6 +3,6 @@
 
 #!/bin/sh 
 
-python2.7 find_sources.py --field S0206+330 --date 54707 --beam 0 --exclude_channels 1369 1370 1371 1372 1373 1374 1375 1376 1377 1378 1379 1380 1381 --verbose --file_verbose --data_filepath /n/fox/data/CALIBN1 
+python2.7 find_sources.py --field S0206+330 --date 54707 --beam 0 --exclude_channels 1369 1370 1371 1372 1373 1374 1375 1376 1377 1378 1379 1380 1381 2046 2047 --verbose --file_verbose --data_filepath /n/fox/data/CALIBN1arecibo 
 
 echo shellDone!
