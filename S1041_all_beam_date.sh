8555eacf6778d35e8db4adffba215742f8c4555c
Joseph Kania <jkania@andrew.cmu.edu>
2014-08-17 20:59:42 -0600

    name changes

--- S1041_all_beam_date.sh
+++ S1041_all_beam_date.sh
@@ -1,21 +1,47 @@
 #Shell script created by Joseph Kania                                                                                                                                                                                        
-#30 June 3014, improved 22 July 2014                                                                                                                                                                                                                
-#!/bin/sh                                                                                                                                                                                                                    
-rm -r ../results/S1041+027
+#30 June 3014, rysync improved 22 July 2014, user specific and cleaned up 16 Aug 2014                                                                                                                                                                                                                
+#!/bin/sh
 
-for i in 55183 55184  55187  55188  55189  55191  55192  55193
-do
-    python2.7 find_sources.py --field S1041+027 --band band0 --date "$i"   --beam 0 1 2 3 4 5 6  --verbose --file_verbose --exclude_channels  2748 2749 2750 2751 2752 2753  --data_filepath /n/fox/processed/S1_CALS --num_source_points 12 --point_sep 1 --num_outer_points 10  --rfi_mask 25 --format ascii
-done 
+echo "Hello $USER"
 
-echo -n "Would you like to transfer the results to AO? [y/n]: "
-read ans
+if [ "$USER" = "jkania" ]; then                                                                                                                                                                                                                     
+    rm -r ../results/S1041+027
 
-case $ans in
-    "y" ) echo "transerring files"
-	rsync -avzcr --delete -e ssh /n/fox/jkania/results* jkania@remote.naic.edu:/share/reu2014/jkania;;
-    * )
-	;;
-esac
+    for i in 55183 55184  55187  55188  55189  55191  55192  55193
+    do
+	python2.7 find_sources.py --field S1041+027 --band band0 --date "$i"  \
+	    --beam 0 1 2 3 4 5 6  --verbose --file_verbose \
+	    --exclude_channels  2748 2749 2750 2751 2752 2753  \
+	    --data_filepath /n/fox/processed/S1_CALS  --format ascii
+    done 
+    echo -n "Would you like to transfer the results to AO? [y/n]: "
+    read ans
+    
+    if [ "$ans" = "y" ]; then
+	echo "transerring files"
+	rsync -avzr --delete -e ssh /n/fox/jkania/results* jkania@remote.naic.edu:/share/reu2014/jkania/
+    fi
+elif [ "$USER" = "tghosh" ]; then 
+    rm -r ../results/S1041+027
+
+    for i in 55183 55184  55187  55188  55189  55191  55192  55193
+    do
+        #python2.7 find_sources.py --field S1041+027 --band band0 --date "$i"  \
+            --beam 0 1 2 3 4 5 6  --verbose --file_verbose \
+            --exclude_channels  2748 2749 2750 2751 2752 2753  \
+            --data_filepath /n/fox/processed/S1_CALS  --format ascii
+    done
+    echo -n "Would you like to transfer the results to AO? [y/n]: "
+    read ans
+
+    if [ "$ans" = "y" ]; then
+        echo "transerring files"
+        #rsync -avzr --delete -e ssh /n/fox/jkania/results* jkania@remote.naic.edu:/share/reu2014/jkania/
+    fi
+else
+    echo "User specific script, you are not recognized"
+fi 
+
+
 
 echo shellDone!
--- test_S1041.sh
+++ test_S1041.sh
@@ -2,19 +2,38 @@
 #30 June 3014, improved 22 July 2014
 
 #!/bin/sh
+echo "Hello $USER"
 
-rm -r ../results_test
+if [ "$USER" = "jkania" ]; then
+    rm -r ../results_test
+ 
+    python2.7 find_sources.py --field S1041+027 --band band0 --date 55183 \
+	--beam 0  --verbose --file_verbose --exclude_channels  2749 2750 2751 2752  \
+	--data_filepath /n/fox/processed/S1_CALS  --results_filepath ../results_test \
+	--format ascii --min_DEC 2.52 --max_DEC 2.92 --min_RA 158.80 --max_RA 161.70
 
-python2.7 find_sources.py --field S1041+027 --band band0 --date 55183 --beam 0  --verbose --file_verbose --exclude_channels  2749 2750 2751 2752  --data_filepath /n/fox/processed/S1_CALS --num_source_points 12 --point_sep 1 --num_outer_points 10 --results_filepath ../results_test --rfi_mask 15 --format ascii --min_DEC 2.52 --max_DEC 2.92 --min_RA 158.80 --max_RA 161.70
+    echo -n "Would you like to transfer the results to AO? [y/n]: "
+    read ans    
+    if [ "$ans" = "y" ]; then
+	echo "transerring files"
+	rsync -avzr --delete -e ssh /n/fox/jkania/results* jkania@remote.naic.edu:/share/reu2014/jkania/
+    fi
+elif [ "$USER" = "tghosh" ]; then 
+    rm -r ../results_test
+    #tghosh's statment
+    #python2.7 find_sources.py --field S1041+027 --band band0 --date 55183 \
+	--beam 0  --verbose --file_verbose --exclude_channels  2749 2750 2751 2752 \
+	--data_filepath /n/fox/processed/S1_CALS  --results_filepath ../results_test \
+	--format ascii --min_DEC 2.52 --max_DEC 2.92 --min_RA 158.80 --max_RA 161.70
 
-echo -n "Would you like to transfer the results to AO? [y/n]: "
-read ans
-
-case $ans in
-    "y" ) echo "transerring files"
-	rsync -avzcr --delete -e ssh /n/fox/jkania/results* jkania@remote.naic.edu:/share/reu2014/jkania/;;
-    * )
-	;;
-esac
-
+    echo -n "Would you like to transfer the results to AO? [y/n]: "
+    read ans
+    if [ "$ans" = "y" ]; then
+        echo "transerring files"
+	#tghosh rsync
+        #rsync -avzr --delete -e ssh /n/fox/jkania/results* jkania@remote.naic.edu:/share/reu2014/jkania/
+    fi
+else 
+    echo "This script is user specific, user not recoginized"
+fi 
 echo shellDone!
